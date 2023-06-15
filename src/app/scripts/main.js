845d0b5571031b72d7e4f64b8c10f24f56328ab2
Stefannya <stefannyaonofre@gmail.com>
2023-06-15 09:35:07 -0500

    first commit

---
+++ src/app/scripts/main.js
@@ -0,0 +1,2 @@
+import "../styles/style.css"
+console.log('hola mundo');
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  mode: "none",
+  entry: {
+    app: ["@babel/polyfill", "./src/app/scripts/main.js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "js/app.bundle.js",
+  },
+  devServer: {
+    port: 5050,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          {
+            loader: "html-loader",
+          }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      inject: "body",
+      hash: true,
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/app.bundle.css",
+    }),
+  ],
+};
